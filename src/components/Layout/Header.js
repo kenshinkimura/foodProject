94ea2713bb282d93f34c3038d247bfbf543d5e7f
Václav Felgr <vaclav.felgr@cleverlance.com>
2023-01-03 14:09:43 +0100

    vse do 137 fixing form input

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -9,7 +9,7 @@ const Header = (props) => {
     <header className={classes.header}> 
         <h1> React Meals</h1>
         <HeaderCartButton></HeaderCartButton>
-        <button>Cart</button>
+        
         </header>
     <div className={classes["main-image"]}>
         <img src={mealsImage} alt="A table full of delicious food"/>
